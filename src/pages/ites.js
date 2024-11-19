0e8bfcdf3fcc6b71a6b70f5370fafd116cedabdb
ishraklatif <81856364+ishraklatif@users.noreply.github.com>
2024-11-19 15:07:07 +0600

    Update ites.js

--- src/pages/ites.js
+++ src/pages/ites.js
@@ -112,7 +112,18 @@ function ITES() {
             </Col>
           ))}
         </Row>
-      </Container>
+      </Container>  <div>
+          {services.map((service, index) => (
+            <div key={index} className="service-detail my-4">
+              <h3>{service.title}</h3>
+              <ul>
+                {service.description.map((desc, i) => (
+                  <li key={i}>{desc}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       <Certificate />
       <Contact />
     </div>
