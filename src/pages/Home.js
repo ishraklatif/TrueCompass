f744eb88558d7ec79f5abe4abae884a04131f5a2
ishraklatif <81856364+ishraklatif@users.noreply.github.com>
2024-12-01 23:11:57 +0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,22 @@
-// Home.js
 import React from 'react';
 import Banner from '../components/Banner';
 import About from './About';
 import Services from '../components/Services';
 import Contact from '../components/Contact';
 import Certificate from '../components/Certificate';
+
 function Home() {
- 
+  const rotateTextArray = [
+    "IT Enabled Services (ITES)",
+    "Outsourcing Services",
+    "BPO Services",
+    "Tech Services",
+    "PR Management",
+  ];
 
   return (
     <div>
-      {/* Pass the toRotate array as a prop */}
-      <Banner />
+      <Banner rotateTextArray={rotateTextArray} showSubtext={true} />
       <About />
       <Certificate />
       <Services />
